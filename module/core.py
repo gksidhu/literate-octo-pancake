988170e29ec7aeaae4e584977f676a5e4744e0c8
Gurnimrat Sidhu <gurnimratsidhu@Gurnimrats-MacBook-Air.local>
2021-07-26 09:32:15 -0400

    added functions

--- module/core.py
+++ module/core.py
@@ -1,9 +1,31 @@
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+import pickle
 
+def train(data: pandas.DataFrame, data_side: pandas.DataFrame, target: str):
+    """Train on data with known labels, returns model"""
+    model.fit(data, target)
+    return model
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+def predict(data: pandas.DataFrame, model: Model):
+    """Generates predictions for unlabeled data, returns list of predictions"""
+    predictions = model.predict(data)
+    return predictions
+
+def metrics(data: pandas.DataFrame, target: str, model: Model):
+    """Generates classification metrics, returns as dictionary"""
+    y_pred = model.predict(data)
+    metrics = classification_report(target, y_pred)
+    return metrics
+
+def save(model: Model, directory: str):
+    """Saves model in specified directory (include filename and extension), returns None"""
+    pickle.dump(model, open(directory, 'wb'))
+    return None 
+
+def load(directory: str):
+    """Loads model from directory, returns model"""
+    model = pickle.load(open(directory, 'rb'))
+    return model
+
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,14 +1,33 @@
 from .context import sample
-
-import unittest
-
-
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_absolute_truth_and_meaning(self):
-        assert True
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+import pickle
+import pytest
 
 
-if __name__ == '__main__':
-    unittest.main()
+def check_train(data: pandas.DataFrame, data_side: pandas.DataFrame, target: str):
+    """Train on data with known labels, returns model"""
+    model.fit(data, target)
+    return model
+
+def check_predict(data: pandas.DataFrame, model: Model):
+    """Generates predictions for unlabeled data, returns list of predictions"""
+    predictions = model.predict(data)
+    return predictions
+
+def check_metrics(data: pandas.DataFrame, target: str, model: Model):
+    """Generates classification metrics, returns as dictionary"""
+    y_pred = model.predict(data)
+    metrics = classification_report(target, y_pred)
+    return metrics
+
+def check_save(model: Model, directory: str):
+    """Saves model in specified directory (include filename and extension), returns None"""
+    pickle.dump(model, open(directory, 'wb'))
+    return None 
+
+def check_load(directory: str):
+    """Loads model from directory, returns model"""
+    model = pickle.load(open(directory, 'rb'))
+    return model
