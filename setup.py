310744df5c3ac676b05e48749d310dd4e271cd0b
Gurnimrat Sidhu <gurnimratsidhu@Gurnimrats-MacBook-Air.local>
2021-07-27 12:11:05 -0400

    Added testing documentation

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import setup, find_packages
+# The find_packages methodconducts a static analysis of libraries included in this package. 
+# It forces install if required libraries are not already present.
 
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
@@ -10,11 +12,12 @@ with open('LICENSE') as f:
 setup(
     name='ML_prod_utils',
     version='0.1.0',
-    description='Template to standardize production ML references',
+    description='Template API interface to deploy machine learning code',
     long_description=readme,
     author='Gurnimrat Sidhu',
     author_email='gurnimratsidhu@gmail.com',
     url='https://github.com/gksighu/literate-octo-pancake',
     license=license,
+    # The below line can be edited to directly list the required libraries, e.g. packages = scikit-learn, pandas, numpy
     packages=find_packages(exclude=('tests'))
 )
